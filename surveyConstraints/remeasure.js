20f3aea3c641a2b47935425fa826578fca286be6
Sergei <poliakos@oregonstate.edu>
2020-02-15 20:17:55 -0800

    I added basic constraints to all of the survey forms. Also I created a new dircetory called surveyConstraints which contains all the JavaScript constraints which are use in the forms.

---
+++ surveyConstraints/remeasure.js
@@ -0,0 +1,6 @@
+// lean angle
+(function() { if (data('lean_angle') <= 100  && data('lean_angle') >=0){return true;} alert('Lean Angle Needs to be Fixed'); return false;})()
+// crown percentage
+(function() { if (data('crown_percentage') <= 100  && data('crown_percentage') >=0){return true;} alert('Crown Ratio Needs to be Fixed'); return false;})()
+// tree percentage
+(function() { if (data('tree_percentage') <= 100  && data('tree_percentage') >=0){return true;} alert('Tree Percentage Needs to be Fixed'); return false;})()
