c5cbf4178d2a56984caef24fd7cf7030eac23fbb
Joseph Landreville <landrevillejoseph@gmail.com>
2020-04-15 11:33:10 -0700

    Make stand/plot inputs on landing page dynamic selects.

--- odk_app/config/assets/js/index.js
+++ odk_app/config/assets/js/index.js
@@ -1,6 +1,8 @@
 $(function () {
   bindPlotSubmit($('#rs-submit'), 'rs', Constants.PlotTypes.REFERENCE_STAND);
   bindPlotSubmit($('#frp-submit'), 'frp', Constants.PlotTypes.FIXED_RADIUS_PLOT);
+
+  loadDataIntoSelects();
 });
 
 function bindPlotSubmit(e, prefix, key)
@@ -18,3 +20,52 @@ function bindPlotSubmit(e, prefix, key)
     odkTables.launchHTML(params, 'config/assets/measure.html')
   });
 }
+
+/////////////////////////////////////////////////////////
+function loadDataIntoSelects()
+{
+  ///////////////////////////////////////////
+  ///////////////////////////////////////////
+  let standSuccess = function (result) {
+    records = {};
+    for (var row = 0; row < result.getCount(); row++) {
+      let stand = result.getData(row, "StandID");
+      records[stand] = stand;
+    }
+    
+    // console.log(records);
+    let st = Utils.genSelectOptions(records);
+    $('#rs_stand').append(st);
+    $('#frp_stand').append(st);
+  }
+  
+  let plotSuccess = function (result) {
+    records = {};
+    for (var row = 0; row < result.getCount(); row++) {
+      let plot = result.getData(row, "Plot");
+      records[plot] = plot;
+    }
+    
+    // console.log(records);
+    let pl = Utils.genSelectOptions(records);
+    $('#frp_plot').append(pl);
+    
+    // if there is only one plot just set it
+    let keys = Object.keys(records);
+    if (keys.length === 1) $('#frp_plot').val(keys[0]);
+  }
+  ///////////////////////////////////////////
+  ///////////////////////////////////////////
+
+  // populate stand selects
+  odkData.arbitraryQuery('prev_data', 'SELECT DISTINCT StandID FROM prev_data', [], null, null, standSuccess, console.log);
+
+  // populate plot select every time frp_stand changes value
+  let frp_stand = $('#frp_stand');
+  frp_stand.change(function () {
+    let plot = $('#frp_plot').html('<option value="">Please select a plot...</option>');
+    odkData.arbitraryQuery('prev_data', 'SELECT DISTINCT Plot FROM prev_data WHERE StandID=?', [frp_stand.val()], null, null, plotSuccess, console.log);
+  });
+}
+
+
